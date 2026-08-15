7b5c0bcba5f4ffb6ea3df0d724b9984d8f4b9fc8
Tushar Singh <singhtushar1801@gmail.com>
2026-08-15 18:00:21 +0530

    Time: 152 ms (32.21%) | Memory: 43.1 MB (28.48%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -7,12 +7,12 @@ class Solution {
         int n = board.length;
         int m = board[0].length;
         int size = word.length();
-        boolean[][] visited = new boolean[n][m];
+        
 
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
                 if(board[i][j] == word.charAt(0)){
-                    if(helper(i,j,board,n,m,1,word,size,visited)){
+                    if(helper(i,j,board,n,m,1,word,size)){
                         return true;
                     }
                 }
@@ -20,23 +20,25 @@ class Solution {
         }
         return false;
     }
-        public boolean helper(int row,int col,char[][] board,int n,int m,int index,String word,int size,boolean[][] visited){
+        public boolean helper(int row,int col,char[][] board,int n,int m,int index,String word,int size){
             if(index == size)
                return true;
-            visited[row][col] = true;
+            
+            char ch = board[row][col];
+            board[row][col] = '$';
 
             for(int i =0;i<4;i++){
                 int ur = row+r[i];
                 int uc = col+c[i];
 
-                if(ur >= 0 && ur < n && uc >= 0 && uc < m && !visited[ur][uc]){
+                if(ur >= 0 && ur < n && uc >= 0 && uc < m ){
                     if(board[ur][uc] == word.charAt(index)){
-                        if(helper(ur,uc,board,n,m,index+1,word,size,visited))
+                        if(helper(ur,uc,board,n,m,index+1,word,size))
                         return true;
                     }
                 }
             }
-            visited[row][col] = false;
+            board[row][col] = ch;
             return false;
         }   
 }
