bdbdacab658c0a96c4cf79d6f86a68c180389c1c
Tushar Singh <singhtushar1801@gmail.com>
2026-08-19 22:04:52 +0530

    Time: 7 ms (68.79%) | Memory: 43.9 MB (85.44%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,21 +1,33 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
-        if(n>m){
+        int n1 = s1.length();
+        int n2 = s2.length();
+
+        if( n1>n2){
             return false;
         }
-        
-        char[] s1Arr = s1.toCharArray();
-        Arrays.sort(s1Arr);
-        for(int i =0;i<=m-n;i++){
-            char[] temp = s2.substring(i,i+n).toCharArray();
-            Arrays.sort(temp);
 
-            if(Arrays.equals(temp,s1Arr)){
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        for(int i =0;i<n1;i++){
+            count1[s1.charAt(i) -'a']++;
+            count2[s2.charAt(i) -'a']++;
+        }
+        if(Arrays.equals(count1,count2)){
+           return true;
+        }
+        int j =0;
+        for(int i =n1;i<n2;i++){
+            count1[s2.charAt(i)-'a']--;
+            count2[s2.charAt(j)-'a']--;
+        
+            if(Arrays.equals(count1,count2)){
                 return true;
             }
+            j++;
         }
-        return false;
+    return false;
     }
+    
 }
